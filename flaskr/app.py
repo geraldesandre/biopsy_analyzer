5a0950379a9ed4c0b7d5784c8304361b2ebec2e3
geraldesandre <geraldes.a.andre@gmail.com>
2021-02-07 18:13:03 +0100

    Cleaned up the code

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,16 +1,9 @@
 
-from flask import Flask, render_template, url_for, request, redirect, session, escape
+from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
-
-
-
-# @app.route('/')
-# def index():
-#     return render_template("index2.html")
 
 
 @app.route('/diagnose_sample', methods=['POST'])
@@ -20,40 +13,12 @@ def diagnose_sample():
 
         print('Calling NEW special POST')
 
-        return 'Return message'
+        return 'The image is now being diagnosed'
 
 
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-
-    if request.method == 'POST':
-
-        print('CALLED POST METHOD')
-
-        if 'modal_visible' in session:
-            session['modal_visible'] = not session['modal_visible']
-        else:
-            session['modal_visible'] = True
-
-        # return "The image is now being diagnosed"
-        return redirect('/')
-
-    else:
-
-        print('CALLED GET METHOD')
-
-        if 'modal_visible' in session:
-            if session['modal_visible']:
-                message = 'Modal is visible'
-                return render_template("index.html", param=message)
-
-        message = 'Modal is NOT visible'
-        return render_template("index.html", param=message)
-
-        # return render_template('index.html', show_predictions_modal=True)
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
