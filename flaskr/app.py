91c3d5aa0ad63d94a40fcde9f54580d6f8a20371
geraldesandre <geraldes.a.andre@gmail.com>
2021-02-07 15:25:32 +0100

    Created a POST method that modified the appearance of the HTML

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,16 +1,34 @@
 
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, session, escape
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
-@app.route('/')
+
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
 
+    if request.method == 'POST':
+        if 'modal_visible' in session:
+            session['modal_visible'] = not session['modal_visible']
+        else:
+            session['modal_visible'] = True
 
+        return redirect('/')
 
+    else:
+
+        if 'modal_visible' in session:
+            if session['modal_visible']:
+                message = 'Modal is visible'
+                return render_template("index.html", param=message)
+
+        message = 'Modal is NOT visible'
+        return render_template("index.html", param=message)
 
 
 if __name__ == "__main__":
