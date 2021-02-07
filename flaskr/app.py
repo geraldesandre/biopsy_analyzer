9508b8e8d551172623a394d54776a6f20f796f49
geraldesandre <geraldes.a.andre@gmail.com>
2021-02-07 18:00:08 +0100

    Created button that calls POST method without refresing the page

--- flaskr/app.py
+++ flaskr/app.py
@@ -13,27 +13,46 @@ app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 #     return render_template("index2.html")
 
 
+@app.route('/diagnose_sample', methods=['POST'])
+def diagnose_sample():
+
+    if request.method == 'POST':
+
+        print('Calling NEW special POST')
+
+        return 'Return message'
+
+
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
+
+        print('CALLED POST METHOD')
+
         if 'modal_visible' in session:
             session['modal_visible'] = not session['modal_visible']
         else:
             session['modal_visible'] = True
 
+        # return "The image is now being diagnosed"
         return redirect('/')
 
     else:
 
+        print('CALLED GET METHOD')
+
         if 'modal_visible' in session:
             if session['modal_visible']:
                 message = 'Modal is visible'
-                return render_template("index2.html", param=message)
+                return render_template("index.html", param=message)
 
         message = 'Modal is NOT visible'
-        return render_template("index2.html", param=message)
+        return render_template("index.html", param=message)
+
+        # return render_template('index.html', show_predictions_modal=True)
 
 
 if __name__ == "__main__":
